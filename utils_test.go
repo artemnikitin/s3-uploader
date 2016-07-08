a67ded3a5bc5cde1f3a0454f9d0de09176eebe25
Artem Nikitin <artemnikitin@artemnikitin.com>
2016-07-08 13:02:29 +0200

    Refactor tests

--- utils_test.go
+++ utils_test.go
@@ -1,153 +1,112 @@
 package main
 
-import (
-	"log"
-	"testing"
-)
+import "testing"
 
-func TestEndsWithPositive(t *testing.T) {
-	str := "dfd/"
-	if !endWith(str, "/") {
-		t.Error("String ends with / should be true")
+func TestEndWith(t *testing.T) {
+	cases := []struct {
+		src, sub string
+		res      bool
+	}{
+		{"dfd/", "/", true},
+		{"dfd/", "d/", true},
+		{"dfd/", "sdssd/", false},
+		{"dfd/", "f", false},
+		{"dfd", "/", false},
+		{"dfd/", "x/", false},
+		{"", "d/", false},
+		{"dfd/", "", true},
+		{"", "", true},
+	}
+
+	for _, v := range cases {
+		result := endWith(v.src, v.sub)
+		if result != v.res {
+			t.Errorf("For string: %s end with: %s, actual: %v, expected: %v", v.src, v.sub, result, v.res)
+		}
 	}
 }
 
-func TestEndsWithBig(t *testing.T) {
-	str := "dfd/"
-	if endWith(str, "sdssd/") {
-		t.Error("String ends with sdssd/ should be false")
+func TestStartWith(t *testing.T) {
+	cases := []struct {
+		src, sub string
+		res      bool
+	}{
+		{"qwe", "q", true},
+		{"qwe", "qw", true},
+		{"qwe", "qwer", false},
+		{"abc", "w", false},
+		{"", "d/", false},
+		{"dfd/", "", true},
+		{"", "", true},
 	}
-}
 
-func TestEndsWithPositive2Symbols(t *testing.T) {
-	str := "dfd/"
-	if !endWith(str, "d/") {
-		t.Error("String ends with d/ should be true")
-	}
-}
-
-func TestEndsWithNegative(t *testing.T) {
-	str := "erwerwe"
-	if endWith(str, "/") {
-		t.Error("String ends with / should be false")
-	}
-}
-
-func TestEndsWithNegative2Symbols(t *testing.T) {
-	str := "erwerwe"
-	if endWith(str, "d/") {
-		t.Error("String ends with d/ should be false")
-	}
-}
-
-func TestStartWithPositive(t *testing.T) {
-	str := "qwe"
-	if !startWith(str, "q") {
-		t.Error("String should start with q")
-	}
-}
-
-func TestStartWithBig(t *testing.T) {
-	str := "qwe"
-	if startWith(str, "qwer") {
-		t.Error("String shouldn't start with qwer")
-	}
-}
-
-func TestStartWithNegative(t *testing.T) {
-	str := "abc"
-	if startWith(str, "w") {
-		t.Error("String shouldn't start with w")
-	}
-}
-
-func TestStartWithPositive2Symbols(t *testing.T) {
-	str := "qwe"
-	if !startWith(str, "qw") {
-		t.Error("String should start with q")
-	}
-}
-
-func TestStartWithNegative2Symbols(t *testing.T) {
-	str := "abc"
-	if startWith(str, "sw") {
-		t.Error("String shouldn't start with w")
+	for _, v := range cases {
+		result := startWith(v.src, v.sub)
+		if result != v.res {
+			t.Errorf("For string: %s start with: %s, actual: %v, expected: %v", v.src, v.sub, result, v.res)
+		}
 	}
 }
 
 func TestGetFolderName(t *testing.T) {
-	path := "/ff/ddd/fff"
-	path2 := "/ff/ddd/fff/"
-	if getFolderName(path) != "fff" {
-		t.Error("Folder name should be fff")
+	cases := []struct{ path, folder string }{
+		{"/ff/ddd/fff", "fff"},
+		{"/ff/ddd/fff/", "fff"},
 	}
-	if getFolderName(path2) != "fff" {
-		t.Error("Folder name should be fff")
+
+	for _, v := range cases {
+		result := getFolderName(v.path)
+		if result != v.folder {
+			t.Errorf("For path: %s folder actual: %s, expected: %s", v.path, result, v.folder)
+		}
 	}
 }
 
 func TestGetPathInsideFolder(t *testing.T) {
-	path := "/fff/ddd/dir/sc/23"
-	path2 := "/fff/ddd/dir/sc/23/"
-	directory := "dir"
-	if getPathInsideFolder(path, directory) != "/dir/sc/23" {
-		t.Error("Path should be /dir/sc/23")
+	cases := []struct{ path, dir, res string }{
+		{"/fff/ddd/dir/sc/23", "dir", "/dir/sc/23"},
+		{"/fff/ddd/dir/sc/23/", "dir", "/dir/sc/23/"},
+		{"/fff/ddd/dir/sc/23/", "qwerty", ""},
 	}
-	if getPathInsideFolder(path2, directory) != "/dir/sc/23/" {
-		t.Error("Path should be /dir/sc/23/")
-	}
-	if getPathInsideFolder(path, "qwerty") != "" {
-		t.Error("Path should be blank")
+
+	for _, v := range cases {
+		result := getPathInsideFolder(v.path, v.dir)
+		if result != v.res {
+			t.Errorf("For path: %s and directory: %s actual output: %s, expected: %s", v.path, v.dir, result, v.res)
+		}
 	}
 }
 
 func TestCreateKey(t *testing.T) {
-	path := "/ddd/qwe/asd.jjj"
-	path2 := "ddd/qwe/asd.jjj"
-
-	if createKey(path) != "/ddd/qwe/asd.jjj" {
-		t.Error("Created key should be equal to", path)
-	}
-	if createKey(path2) != "/ddd/qwe/asd.jjj" {
-		t.Error("Created key should be equal to", path)
+	cases := []struct{ uploadPath, path, res string }{
+		{"", "/ddd/qwe/asd.jjj", "/ddd/qwe/asd.jjj"},
+		{"", "ddd/qwe/asd.jjj", "/ddd/qwe/asd.jjj"},
+		{"/vvv/ddd/", "/ddd/qwe/asd.jjj", "/vvv/ddd/ddd/qwe/asd.jjj"},
+		{"/vvv/ddd/", "ddd/qwe/asd.jjj", "/vvv/ddd/ddd/qwe/asd.jjj"},
+		{"/vvv/ddd", "ddd/qwe/asd.jjj", "/vvv/ddd/ddd/qwe/asd.jjj"},
 	}
 
-	*uploadpath = "/vvv/ddd/"
-	if createKey(path) != "/vvv/ddd/ddd/qwe/asd.jjj" {
-		t.Error("Created key should be equal to", *uploadpath+path2)
-	}
-	if createKey(path2) != "/vvv/ddd/ddd/qwe/asd.jjj" {
-		t.Error("Created key should be equal to", *uploadpath+path2)
-	}
-
-	*uploadpath = "/vvv/ddd"
-	if createKey(path) != "/vvv/ddd/ddd/qwe/asd.jjj" {
-		t.Error("Created key should be equal to", *uploadpath+path)
-	}
-	if createKey(path2) != "/vvv/ddd/ddd/qwe/asd.jjj" {
-		t.Error("Created key should be equal to", *uploadpath+path)
+	for _, v := range cases {
+		*uploadpath = v.uploadPath
+		result := createKey(v.path)
+		if result != v.res {
+			t.Errorf("For path: %s actual key: %s, expected: %s", v.path, result, v.res)
+		}
 	}
 }
 
 func TestGetFileName(t *testing.T) {
-	*rename = ""
-
-	path := getFileName("dfd.kkk")
-	if path != "dfd.kkk" {
-		log.Println("path =", path)
-		t.Error("Should return dfd.kkk for file")
+	cases := []struct{ rename, path, res string }{
+		{"", "dfd.kkk", "dfd.kkk"},
+		{"", "/dfdfd/www/dfd.kkk", "dfd.kkk"},
+		{"dfg.lll", "/dfdfd/www/dft.kkk", "dfg.lll"},
 	}
 
-	path = getFileName("/dfdfd/www/dfd.kkk")
-	if path != "dfd.kkk" {
-		log.Println("path =", path)
-		t.Error("Should return dfd.kkk for path")
-	}
-
-	*rename = "dfg.lll"
-	path = getFileName("/dfdfd/www/dfd.kkk")
-	if path != "dfg.lll" {
-		log.Println("path =", path)
-		t.Error("Should return dfg.lll if rename option is specified")
+	for _, v := range cases {
+		*rename = v.rename
+		result := getFileName(v.path)
+		if result != v.res {
+			t.Errorf("For path: %s actual filename: %s, expected: %s", v.path, result, v.res)
+		}
 	}
 }
